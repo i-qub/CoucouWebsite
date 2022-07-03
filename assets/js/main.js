dff6e5f1eec423ba032b25dbac79d07f0ba9b688
Andhavarapu Rahul <andhavarapurahul@gmail.com>
2022-07-03 15:58:44 +0530

    loader

--- assets/js/main.js
+++ assets/js/main.js
@@ -239,3 +239,13 @@
   });
 
 })()
+
+window.addEventListener("load", () => {
+  const loader = document.querySelector(".pre-loader");
+
+  loader.classList.add("pre-loader--hidden");
+
+  loader.addEventListener("transitionend", () => {
+    document.body.removeChild(loader);
+  });
+});
